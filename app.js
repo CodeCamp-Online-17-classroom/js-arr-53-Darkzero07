6f73ef41a2b401de5302bafe99c0628f8f136512
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 18:47:33 +0700

    js lab 139

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// write code here
+// จงหาผลรวมของเลขที่มีค่าน้อยกว่า 40 ที่อยู่ในตัวแปร str
+
+let str = "31 45 12 67 34 86 23 37 19 41";
+
+const arrStr = str.split(" ").map(Number);
+const numberLower40 = arrStr.filter((arr) => arr < 40);
+console.log(numberLower40.reduce((sum, arr) => sum + arr));
